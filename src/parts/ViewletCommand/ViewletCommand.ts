c8d7678b8d155024c797308a837419baee87d049
Le Vivilet <levivlet@pm.me>
2025-08-30 16:11:35 +0200

    feature: add viewlet commands

---
+++ src/parts/ViewletCommand/ViewletCommand.ts
@@ -0,0 +1,4 @@
+export const FocusElementByName = 'Viewlet.focusElementByName'
+export const SetFocusContext = 'Viewlet.setFocusContext'
+export const FocusSelector = 'Viewlet.focusSelector'
+export const SetDom2 = 'Viewlet.setDom2'
