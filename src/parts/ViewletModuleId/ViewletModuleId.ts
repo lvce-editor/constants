1733e6284f8aa862a06a83d3fb802c076dc79acf
Le Vivilet <levivilet@pm.me>
2025-10-27 17:14:22 +0100

    feature: add viewlet module id

---
+++ src/parts/ViewletModuleId/ViewletModuleId.ts
@@ -0,0 +1,9 @@
+export const Explorer = 'Explorer'
+
+export const Extensions = 'Extensions'
+
+export const RunAndDebug = 'Run And Debug'
+
+export const Search = 'Search'
+
+export const SourceControl = 'Source Control'
