8e7b2bb600a7735adae34871c801ea7d0ec6d39d
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-11-17 15:10:20 +0100

    feature: add updateState constant

---
+++ src/parts/UpdateState/UpdateState.ts
@@ -0,0 +1,6 @@
+export const None = 0
+export const CheckingForUpdate = 1
+export const DownloadingUpdate = 2
+export const DownloadedUpdate = 3
+export const WaitingForRestart = 4
+export const InstallingUpdated = 5
