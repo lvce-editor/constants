5a787dcb81a5fae21f1584f72782e17d18b8d8c8
Le Vivilet <levivilet@pm.me>
2025-12-11 18:39:46 +0100

    feature: add quickpick entry id (#77)

---
+++ src/parts/QuickPickEntryId/QuickPickEntryId.ts
@@ -0,0 +1,23 @@
+export const ColorTheme = 0
+
+export const Commands = 1
+
+export const Custom = 2
+
+export const File = 3
+
+export const GoToLine = 4
+
+export const Help = 5
+
+export const Recent = 6
+
+export const Symbol = 7
+
+export const View = 8
+
+export const WorkspaceSymbol = 9
+
+export const Noop = 10
+
+export const EveryThing = 100
