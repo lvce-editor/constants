d6763d85c3d38fa80ed8b2dc3fcd8e2070a74491
Le Vivilet <levivilet@pm.me>
2026-03-08 11:32:28 +0100

    feature: add ChatToolWorker constant to RpcId (#126)

--- src/parts/RpcId/RpcId.ts
+++ src/parts/RpcId/RpcId.ts
@@ -1,5 +1,6 @@
 export const ChatNetworkWorker = 6002
 export const ChatStorageWorker = 6003
+export const ChatToolWorker = 6005
 export const Cli = 6004
 export const ClipBoardProcess = 3401
 export const ClipBoardWorker = 3400
