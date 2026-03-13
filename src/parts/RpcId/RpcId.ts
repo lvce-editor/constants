043066364ff2527e1923aca57c12b9a481032429
Le Vivilet <levivilet@pm.me>
2026-03-13 23:34:20 +0100

    feature: add ChatMathWorker constant to RpcId (#137)

--- src/parts/RpcId/RpcId.ts
+++ src/parts/RpcId/RpcId.ts
@@ -2,6 +2,7 @@ export const ChatNetworkWorker = 6002
 export const ChatStorageWorker = 6003
 export const ChatToolWorker = 6005
 export const ChatDebugWorker = 6006
+export const ChatMathWorker = 6007
 export const Cli = 6004
 export const ClipBoardProcess = 3401
 export const ClipBoardWorker = 3400
