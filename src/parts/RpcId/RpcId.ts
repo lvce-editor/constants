3c4a2c3282f68ae9256b65bec04534682804b68e
Le Vivilet <levivilet@pm.me>
2026-08-05 02:47:56 -0700

    feature: add drag and drop worker rpc id (#177)

--- src/parts/RpcId/RpcId.ts
+++ src/parts/RpcId/RpcId.ts
@@ -16,6 +16,7 @@ export const ColorPickerWorker = 302
 export const CompletionWorker = 301
 export const DebugWorker = 55
 export const DialogWorker = 7014
+export const DragAndDropWorker = 3404
 export const EditorWorker = 99
 export const EmbedsProcess = 207
 export const EmbedsWorker = 208
